78eecba416e990646eb1fc4ca188012c18cb0c3c
petroffn <petroff.n@gmail.com>
2021-07-14 14:39:00 +0300

    Tests for Browser Windows

--- test_AFW_BrowserWindows.py
+++ test_AFW_BrowserWindows.py
@@ -8,60 +8,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 @allure.title('Test')
 @allure.severity(Severity.BLOCKER)
 
-def test_AFW_01():
+def test_AFW_BW_01():
     driver = webdriver.Chrome()
     with allure.step('Open Practice Form page'):
-        driver.get('https://demoqa.com/alerts')
+        driver.get('https://demoqa.com/browser-windows')
         driver.set_window_size(1920, 1080)
 
-    with allure.step('Open Click me 1'):
-        clickme1 = driver.find_element_by_xpath('//button[@id="alertButton"]')
-        clickme1.click()
+    with allure.step('Open new tab'):
+        newtab = driver.find_element_by_xpath('//button[@id="tabButton"]')
+        newtab.click()
 
-        WebDriverWait(driver, 10).until(EC.alert_is_present())
-        driver.switch_to.alert.accept()
+    driver.switch_to.window(driver.window_handles[1])
+    with allure.step('Check title'):
+        assert "'This is a sample page'"
 
-    with allure.step('OOpen Click me 2'):
-        clickme2 = driver.find_element_by_xpath('//button[@id="timerAlertButton"]')
-        clickme2.click()
+def test_AFW_BW_02():
+    driver = webdriver.Chrome()
+    with allure.step('Open Practice Form page'):
+        driver.get('https://demoqa.com/browser-windows')
+        driver.set_window_size(1920, 1080)
 
-        import time
-        time.sleep(6)
+    with allure.step('Open new window'):
+        newwindow = driver.find_element_by_xpath('//button[@id="windowButton"]')
+        newwindow.click()
 
-        WebDriverWait(driver, 10).until(EC.alert_is_present())
-        driver.switch_to.alert.accept()
+    driver.switch_to.window(driver.window_handles[1])
+    with allure.step('Check title'):
+        assert "'This is a sample page'"
 
-    with allure.step('Open Click me 3'):
-        clickme3 = driver.find_element_by_xpath('//button[@id="confirmButton"]')
-        clickme3.click()
-        WebDriverWait(driver, 10).until(EC.alert_is_present())
-        driver.switch_to.alert.accept()
-        assert "'You selected Cancel'" not in driver.page_source
+def test_AFW_BW_03():
+    driver = webdriver.Chrome()
+    with allure.step('Open Practice Form page'):
+        driver.get('https://demoqa.com/browser-windows')
+        driver.set_window_size(1920, 1080)
 
-    with allure.step('Open Click me 4'):
-        clickme3 = driver.find_element_by_xpath('//button[@id="promtButton"]')
-        clickme3.click()
+    with allure.step('Open new window'):
+        newmessage = driver.find_element_by_xpath('//button[@id="messageWindowButton"]')
+        newmessage.click()
 
-        obj = driver.switch_to.alert
-        obj.send_keys('Mykola')
-
-        import time
-        time.sleep(2)
-
-        obj.accept()
-
-        message = obj.text
-        print("Alert shows following message: " + message)
-
-        time.sleep(2)
-
-        obj.accept()
-
-        # get the text returned when OK Button is clicked.
-        txt = driver.find_element_by_id('msg')
-        print(txt.text)
-
-        #WebDriverWait(driver, 10).until(EC.alert_is_present())
-        #driver.switch_to.alert.accept()
-        assert "'You entered Mykola'" not in driver.page_source
+        driver.switch_to.window(driver.window_handles[1])
+        with allure.step('Check title'):
+            assert "'Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.'"
 
